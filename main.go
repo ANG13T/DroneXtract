f35ed59c89fbcdc0c020f4865d394a43f0c2daa3
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-15 14:06:58 -0700

    feat:update color of gradient

--- main.go
+++ main.go
@@ -16,12 +16,10 @@ func Banner() {
 	banner, _ := ioutil.ReadFile("txt/banner.txt")
 	info, _ := ioutil.ReadFile("txt/info.txt")
 	options, _ := ioutil.ReadFile("txt/options.txt")
-	g,_:=gradient.NewGradient("cyan", "blue")
-	solid,_:=gradient.NewGradient("blue", "#1179ef")
-	opt,_:=gradient.NewGradient("#1179ef", "cyan")
+	g,_:=gradient.NewGradient("#1179ef", "cyan")	
 	g.Print(string(banner))
-	solid.Print(string(info))
-	opt.Print("\n" + string(options) + "\n")
+	fmt.Println(color.Ize(color.Cyan, string(info)))
+	fmt.Println(color.Ize(color.Cyan, "\n" + string(options) + "\n"))
 }
 
 func Option() {
