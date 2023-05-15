8d7841de64b57265f1ee7ff28660ad60e9f22bbe
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-15 13:30:02 -0700

    feat: init options text file

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ import (
 func Banner() {
 	banner, _ := ioutil.ReadFile("txt/banner.txt")
 	info, _ := ioutil.ReadFile("txt/info.txt")
-	// options, _ := ioutil.ReadFile("txt/options.txt")
+	options, _ := ioutil.ReadFile("txt/options.txt")
 	g,_:=gradient.NewGradient("cyan", "blue")
 	solid,_:=gradient.NewGradient("blue", "#1179ef")
-	// opt,_:=gradient.NewGradient("#1179ef", "cyan")
+	opt,_:=gradient.NewGradient("#1179ef", "cyan")
 	g.Print(string(banner))
 	solid.Print(string(info))
-	// opt.Print("\n" + string(options))
+	opt.Print("\n" + string(options))
 }
 
 func main() {
