25ca070ce86d8e37d693b650a5e2dd2b5121efca
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-15 18:58:07 -0700

    feat: check build for `ParseDatToCSV`

--- forensics/datparser.go
+++ forensics/datparser.go
@@ -4,6 +4,11 @@ import (
 	"io/ioutil"
 	"github.com/TwiN/go-color"
 	"fmt"
+	"os"
+	"io"
+	// "bufio"
+	"unsafe"
+	"log"
 )
 
 
@@ -24,7 +29,62 @@ func DATParser() {
 }
 
 func ParseDatToCSV() {
+	fmt.Print("\n ENTER DAT FILE PATH > ")
+	var path string
+	fmt.Scanln(&path)
+	_, err := os.Open(path)
+	// TODO: change this
+	force := false
+	//outFn := "../output/output.csv"
+ 
+	if err != nil {
+		fmt.Println(color.Ize(color.Red, "  [!] INVALID DAT FILE"))
+	}
 
+	inFile, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inFile.Close()
+
+	var fileHeader [128]byte
+	_, err = inFile.Read(fileHeader[:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buildBytes := *(*[5]byte)(unsafe.Pointer(&fileHeader[16])) // Read 5 bytes starting from offset 16
+	build := string(buildBytes[:])
+
+	if build != "BUILD" {
+		if !force {
+			err := NotDATFileError{filename: path}
+			log.Fatal(err)
+		} else {
+			fmt.Printf("*** WARNING: %s is not a recognized DJI DAT file but will be processed anyway because the FORCE flag was set. ***\n", path)
+			_, err = inFile.Seek(0, io.SeekStart) // set the pointer to the beginning of the file because this is an unrecognized file type and we don't want to risk missing data
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	// outFile, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer outFile.Close()
+
+	// var byteValue [1]byte
+	// _, err = io.ReadFull(inFile, byteValue[:])
+	// if err != nil {
+	// 	fmt.Println(color.Ize(color.Red, "  [!] INVALID DAT FILE"))
+	// }
+
+	// if byteValue[0] != 0x55 {
+	// 	alternateStructure := true
+	// }
+	
 }
 
 func ParseDatToTXT() {
--- forensics/errors.go
+++ forensics/errors.go
@@ -2,6 +2,7 @@ package forensics
 
 import (
 	"fmt"
+	"io"
 )
 
 type NotDATFileError struct {
@@ -10,4 +11,53 @@ type NotDATFileError struct {
 
 func (e NotDATFileError) Error() string {
 	return fmt.Sprintf("*** ERROR: %s is not a recognized DJI DAT file. ***", e.filename)
+}
+
+type NoNewPacketError struct {
+	byteValue byte
+	filePos   int64
+}
+
+func (e NoNewPacketError) Error() string {
+	return fmt.Sprintf("No new packet found. Byte: 0x%x, File Position: %d", e.byteValue, e.filePos)
+}
+
+// CorruptPacketError represents a custom error for a corrupt packet.
+type CorruptPacketError struct {
+	message string
+}
+
+func (e CorruptPacketError) Error() string {
+	return e.message
+}
+
+// Message represents the message structure.
+type Message struct {
+	// Define the fields of the Message structure as needed
+}
+
+func (m *Message) setTickNo(tickNo int) {
+	// Implement the setTickNo method as needed
+}
+
+func (m *Message) writeRow(writer io.Writer, tickNo int) {
+	// Implement the writeRow method as needed
+}
+
+func (m *Message) addPacket(pktLen int, header, payload []byte) bool {
+	// Implement the addPacket method as needed
+	return false
+}
+
+func (m *Message) getRow() []string {
+	// Implement the getRow method as needed
+	return nil
+}
+
+func (m *Message) writeKml(row []string) {
+	// Implement the writeKml method as needed
+}
+
+func (m *Message) finalizeKml() {
+	// Implement the finalizeKml method as needed
 }
