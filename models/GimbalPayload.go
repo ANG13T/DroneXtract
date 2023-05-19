4c99f6eff16e1c7575913e81f4ef7ec3c6afd8e4
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-19 02:29:07 -0700

    fix: all compilation issues for `GimbalPayload`

--- models/GimbalPayload.go
+++ models/GimbalPayload.go
@@ -28,23 +28,28 @@ func NewGimbalPayload(payload []byte) *GimbalPayload {
 	return g
 }
 
+func convert_rad_to_degrees(val float64) float64 {
+	return val * 180.0 / math.Pi
+}
+
+
 func (g *GimbalPayload) parse() {
 	data := make(map[string]interface{})
 
-	quatW := math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[78:82]))
-	quatX := math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[82:86]))
-	quatY := math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[86:90]))
-	quatZ := math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[90:94]))
+	quatW := math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[78:82]))
+	quatX := math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[82:86]))
+	quatY := math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[86:90]))
+	quatZ := math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[90:94]))
 
-	qGimbal := Quaternion{W: quatW, X: quatX, Y: quatY, Z: quatZ}
-	rpy := qGimbal.toEuler()
-	data["Gimbal:Xpitch"] = math.Degrees(rpy[0])
-	data["Gimbal:Xroll"] = math.Degrees(rpy[1])
-	data["Gimbal:Xyaw"] = math.Degrees(rpy[2])
+	qGimbal := Quaternion{Scalar: quatW, X: quatX, Y: quatY, Z: quatZ}
+	rpy := qGimbal.ToEuler()
+	data["Gimbal:Xpitch"] = convert_rad_to_degrees(rpy[0])
+	data["Gimbal:Xroll"] = convert_rad_to_degrees(rpy[1])
+	data["Gimbal:Xyaw"] = convert_rad_to_degrees(rpy[2])
 
-	data["Gimbal:yaw"] = math.Degrees(math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[94:98])))
-	data["Gimbal:roll"] = math.Degrees(math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[98:102])))
-	data["Gimbal:pitch"] = math.Degrees(math.Float32frombits(binary.LittleEndian.Uint32(g.Payload[102:106])))
+	data["Gimbal:yaw"] = convert_rad_to_degrees(math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[94:98])))
+	data["Gimbal:roll"] = convert_rad_to_degrees(math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[98:102])))
+	data["Gimbal:pitch"] = convert_rad_to_degrees(math.Float64frombits(binary.LittleEndian.Uint64(g.Payload[102:106])))
 
 	data["rFront"] = int16(binary.LittleEndian.Uint16(g.Payload[219:221]))
 	data["lFront"] = int16(binary.LittleEndian.Uint16(g.Payload[221:223]))
