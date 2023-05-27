21911cf66385cba72c56837ceec332044e0f52fa
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-26 21:31:26 -0700

    feat: init DJI_SRT_Parser

--- steganographics/SRTAnalysis.go
+++ steganographics/SRTAnalysis.go
@@ -1,1 +1,122 @@
 package steganographics
+
+// subtitle extratoir
+// SRT Viewer
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var isoDateRegex = regexp.MustCompile(`[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?Z`)
+
+type DJI_SRT_Parser struct {
+	fileName        string
+	metadata        map[string]interface{}
+	rawMetadata     []interface{}
+	smoothened      int
+	millisecondsSample int
+	loaded          bool
+	isMultiple      bool
+	customProperties map[string]interface{}
+}
+
+func (p *DJI_SRT_Parser) srtToObject(srt string) []map[string]interface{} {
+	maybeParseNumbers := func(d string) interface{} {
+		if isNum(d) {
+			num, err := strconv.ParseFloat(d, 64)
+			if err != nil {
+				return d
+			}
+			return num
+		}
+		return d
+	}
+
+	// Convert SRT strings file into an array of objects
+	converted := make([]map[string]interface{}, 0)
+	timecodeRegEx := regexp.MustCompile(`(\d{2}:\d{2}:\d{2},\d{3})\s-->\s`)
+	packetRegEx := regexp.MustCompile(`^\d+$`)
+	arrayRegEx := regexp.MustCompile(`\b([A-Z_a-z]+)\(([-\+\w.,/]+)\)`)
+	valueRegEx := regexp.MustCompile(`\b([A-Z_a-z]+)\s?:[\s\[a-z_A-Z\]]?([-\+\d./]+)\w{0,3}\b`)
+	dateRegEx := regexp.MustCompile(`\d{4}[-.]\d{1,2}[-.]\d{1,2} \d{1,2}:\d{2}:\d{2,}`)
+	accurateDateRegex := regexp.MustCompile(`(\d{4}[-.]\d{1,2}[-.]\d{1,2} \d{1,2}:\d{2}:\d{2}),(\w{3}),(\w{3})`)
+	accurateDateRegex2 := regexp.MustCompile(`(\d{4}[-.]\d{1,2}[-.]\d{1,2} \d{1,2}:\d{2}:\d{2})[,.](\w{3})`)
+	isDJIFPV := regexp.MustCompile(`font size="28"`).MatchString(srt) &&
+		regexp.MustCompile(`\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{2}:\d{2}.\d{3}`).MatchString(srt) &&
+		regexp.MustCompile(`\[altitude: \d.*\]`).MatchString(srt)
+
+	// Split difficult Phantom4Pro format
+	srt = strings.ReplaceAll(srt, ",", ":separator:")
+	srt = regexp.MustCompile(`\(([^\)]+)\)`).ReplaceAllStringFunc(srt, func(match string) string {
+		match = strings.ReplaceAll(match, ",", ":separator:")
+		match = strings.ReplaceAll(match, " ", "")
+		return match
+	})
+	srt = strings.ReplaceAll(srt, ", ", " ")
+	srt = strings.ReplaceAll(srt, "Â", "")
+	srt = strings.ReplaceAll(srt, "°", "")
+	srt = strings.ReplaceAll(srt, "B0", "")
+	lines := strings.Split(srt, "\n")
+	srtLines := make([]string, 0)
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) > 0 {
+			srtLines = append(srtLines, line)
+		}
+	}
+
+	for _, line := range srtLines {
+		var match []string
+		if packetRegEx.MatchString(line) {
+			// New packet
+			converted = append(converted, make(map[string]interface{}))
+		} else if m := timecodeRegEx.FindStringSubmatch(line); len(m) > 0 {
+			converted[len(converted)-1]["TIMECODE"] = m[1]
+		} else {
+			for _, m = range arrayRegEx.FindAllStringSubmatch(line, -1) {
+				key := m[1]
+				values := strings.Split(m[2], ",")
+				parsedValues := make([]interface{}, len(values))
+				for i, v := range values {
+					parsedValues[i] = maybeParseNumbers(v)
+				}
+				converted[len(converted)-1][key] = parsedValues
+			}
+			for _, m = range valueRegEx.FindAllStringSubmatch(line, -1) {
+				key := m[1]
+				value := maybeParseNumbers(m[2])
+				converted[len(converted)-1][key] = value
+			}
+			if m := isoDateRegex.FindStringSubmatch(line); len(m) > 0 {
+				converted[len(converted)-1]["DATE"] = line
+			} else if m := accurateDateRegex.FindStringSubmatch(line); len(m) > 0 {
+				converted[len(converted)-1]["DATE"] = m[1] + ":" + m[2] + "." + m[3]
+			} else if m := accurateDateRegex2.FindStringSubmatch(line); len(m) > 0 {
+				converted[len(converted)-1]["DATE"] = m[1] + "." + m[2]
+			} else if m := dateRegEx.FindStringSubmatch(line); len(m) > 0 {
+				converted[len(converted)-1]["DATE"] = strings.ReplaceAll(m[0], ":"+m[2], "."+m[2])
+			} else if isDJIFPV && regexp.MustCompile(`\[altitude: \d.*\]`).MatchString(line) {
+				// Correct altitude divided by 10 problem in DJI FPV drone
+				altitude := converted[len(converted)-1]["altitude"].(string)
+				altitudeValue, err := strconv.Atoi(altitude)
+				if err == nil {
+					converted[len(converted)-1]["altitude"] = strconv.Itoa(altitudeValue * 10)
+				}
+			}
+		}
+	}
+
+	if len(converted) < 1 || len(converted[0]) == 0 {
+		fmt.Println("Error converting object")
+		return nil
+	}
+	return converted
+}
+	
+func isNum(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
