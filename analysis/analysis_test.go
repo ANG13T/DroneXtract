ad3ff3e50a25216b53cd713c34630c23a813e36e
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-07-16 14:04:12 -0700

    feat: init analysis testing suite

--- analysis/analysis_test.go
+++ analysis/analysis_test.go
@@ -1,2 +1,20 @@
 package analysis
 
+import (
+	"testing"
+	"github.com/TwiN/go-color"
+	"fmt"
+)
+
+func TestDJIAnalysis(t *testing.T) {
+	t.Logf("Running Analysis Test Unit - CSV File")
+	fmt.Println(color.Ize(color.Cyan, "Running Telemetry Test Unit - CSV Files"))
+
+	filename := `../test-data/Airdata-Files/AirdataCSV.csv`
+
+	suite := NewDJI_Analysis(filename)
+
+	suite.RunAnalysis()
+
+	t.Logf("[Parsing #1] CSV Analysis Case - PASS")
+}
